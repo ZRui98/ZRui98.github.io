26f238e5ddcf4ac6aa797e2a05b730f7c469f1d8
zrui98 <ciguatera.z.rui@gmail.com>
2021-04-27 00:05:48 -0400

    Fix resume typo

--- minify.sh
+++ minify.sh
@@ -3,6 +3,7 @@ CSS_FILES=()
 while IFS= read -r -d $'\0'; do
     CSS_FILES+=("$REPLY")
 done < <(find $1 -name "*.css" -print0)
+
 for file in "${CSS_FILES[@]}"; do
     MINIFIED="$(cat "$file" \
         | tr -d '\t' | tr -d '\r\n' \
